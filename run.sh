312b948dd7997ec1db7d289e7be91e2295f4035f
Marius <gbhrdt@users.noreply.github.com>
2020-11-07 01:11:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ S3BACKUP=${S3PATH}\${BACKUP_NAME}
 S3LATEST=${S3PATH}latest.dump.gz
 aws configure set default.s3.signature_version s3v4
 echo "=> Backup started"
-if mongodump --uri ${MONGODB_URI}${DB_STR} --archive=\${BACKUP_NAME} --gzip ${EXTRA_OPTS} && aws s3 cp \${BACKUP_NAME} \${S3BACKUP} ${REGION_STR} ${STORAGE_CLASS_STR} && aws s3 cp \${S3BACKUP} \${S3LATEST} ${REGION_STR} ${STORAGE_CLASS_STR} && rm \${BACKUP_NAME} ;then
+if mongodump --uri '${MONGODB_URI}${DB_STR}' --archive=\${BACKUP_NAME} --gzip ${EXTRA_OPTS} && aws s3 cp \${BACKUP_NAME} \${S3BACKUP} ${REGION_STR} ${STORAGE_CLASS_STR} && aws s3 cp \${S3BACKUP} \${S3LATEST} ${REGION_STR} ${STORAGE_CLASS_STR} && rm \${BACKUP_NAME} ;then
     echo "   > Backup succeeded"
 else
     echo "   > Backup failed"
